03820a2d0a2343358614a13d47e48e9c5bc9af02
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-11 11:08:48 +0100

    insert nav items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,32 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <>
+            <nav className="nav-wrapper">
+                <div className="nav-content">
+                    <img src="https://picsum.photos/100" alt="nick mbk logo" className="logo" />
+
+                    <ul>
+                        <li>
+                            <a className="menu-item">Home</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Skillse</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Work Experience</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Contact Me</a>
+                        </li>
+
+                        <button className="contact-btn" onClick={() => {}}>
+                            Hire Me
+                        </button>
+                    </ul>
+                </div>
+            </nav>
+        </>
     )
 };
 
