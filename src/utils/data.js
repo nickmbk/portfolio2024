671e9b236b26c67a7637f4f671da12011b4cb089
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-12 19:59:00 +0100

    add work experience data to the js data

--- src/utils/data.js
+++ src/utils/data.js
@@ -36,3 +36,39 @@ export const SKILLS = [
         ],
     },
 ]
+
+export const WORK_EXPERIENCE = [
+    {
+        title: "Web Developer (Part-time freelance)",
+        company: "Ziggy PrintIT",
+        date: "October 2023 - Present",
+        responsibilities:[
+            "Building projects in WordPress based on company needs.",
+            "Create custom themes in WordPress using a combination of PHP, HTML, CSS, Sass, JavaScript and jQuery for the companies.",
+            "Continuous development of sites and AI projects",
+            "Regular team meetings to present new development and discuss further developments.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company: "Royal Wares",
+        date: "May 2023 - October 2023",
+        responsibilities:[
+            "Create websites in WordPress based on customer needs.",
+            "Create custom themes in WordPress using a combination of PHP, HTML, CSS, JavaScript and Vue, to the customers requirements.",
+            "Troubleshoot and debug any problems using available tools.",
+            "Collaborate and communicate with colleagues and clients to produce the best websites possible.",
+        ],
+    },
+    {
+        title: "Despatch Operative",
+        company: "Oldham Seals",
+        date: "September 2012 - May 2023",
+        responsibilities:[
+            "Prepare and pack orders in fast paced environment, with accuracy and high attention to detail.",
+            "Organise and prioritise orders to be despatched to the customer to meet agreed delivery date.",
+            "Communicate with other departments to supply products in order to meet despatch deadlines.",
+            "Train new members of staff to a high standard to maintain high level of customer satisfaction.",
+        ],
+    },
+]
