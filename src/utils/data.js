843b9bf298a19417ddab7cf74ccfd95900f81f62
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-12 17:25:00 +0100

    create a data file to contain skills information

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/images/icons/frontend.svg",
+        skills: [
+            { skill: "HTML", percentage: "80%" },
+            { skill: "CSS & SASS", percentage: "80%" },
+            { skill: "JavaScript", percentage: "60%" },
+            { skill: "React", percentage: "40%" },
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/images/icons/backend.svg",
+        skills: [
+            { skill: "Node JS", percentage: "50%" },
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "./assets/images/icons/tools.svg",
+        skills: [
+            { skill: "Git & GitHub", percentage: "80%" },
+            { skill: "Visual Studio Code", percentage: "70%" },
+            { skill: "Responsive Design", percentage: "60%" },
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/images/icons/soft-skills.svg",
+        skills: [
+            { skill: "Problem-solving", percentage: "60%" },
+            { skill: "Attention to Detail", percentage: "70%" },
+            { skill: "Organisation", percentage: "80%" },
+            { skill: "Communication", percentage: "40%" },
+        ],
+    },
+]
