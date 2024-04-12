8c47100577e25ecd630ed9f1cd83aab388f3c324
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-12 16:04:02 +0100

    edit hero contents

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,35 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <section className="hero-container">
+            <div className="hero-content">
+                <h2>Building Digital Experiences That Inspire</h2>
+                <p>
+                    Passionate Frontend Developer | Transforming Ideas into Seamless and Visually Stunning Web Solutions
+                </p>
+            </div>
+
+            <div className="hero-img">
+                <div>
+                    <div className="tech-icon">
+                        <img src="https://picsum.photos/400" alt="" />
+                    </div>
+                    <img src="https://picsum.photos/200" alt="" />
+                </div>
+
+                <div>
+                    <div className="tech-icon">
+                        <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                    <div className="tech-icon">
+                        <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                    <div className="tech-icon">
+                        <img src="https://picsum.photos/200" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
