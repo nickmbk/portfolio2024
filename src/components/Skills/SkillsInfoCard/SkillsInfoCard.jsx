c5ff6468908b7528f6d6b106b4a123df846ecd12
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-12 18:42:35 +0100

    create SkillsInfoCard jsx and css files, add basic code to jsx file

---
+++ src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SkillsInfoCard.css';
+
+const SkillsInfoCard = () => {
+    return (
+        <div>SkillsInfoCard</div>
+    );
+};
+
+export default SkillsInfoCard;
