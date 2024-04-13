cbebb30c24ca2c1bf67277bd5f06d840eb2f97e3
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-13 16:48:13 +0100

    create basic structure for the contact section

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -3,7 +3,14 @@ import './ContactMe.css';
 
 const ContactMe = () => {
     return (
-        <div>ContactMe</div>
+        <section className="contact-container">
+            <h5>Contact Me</h5>
+
+            <div className="contact-content">
+                <div style={{ flex: 1 }}></div>
+                <div style={{ flex: 1 }}></div>
+            </div>
+        </section>
     );
 };
 
