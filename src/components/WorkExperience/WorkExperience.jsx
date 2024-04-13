2823bceb963ba561bf102b8ba26ea244c88d81df
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-13 14:11:42 +0100

    create containers for experience content

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -3,7 +3,13 @@ import './WorkExperience.css';
 
 const WorkExperience = () => {
     return (
-        <div>Work Experience</div>
+        <section className="experience-container">
+            <h5>Work Experience</h5>
+
+            <div className="experience-content">
+                
+            </div>
+        </section>
     )
 }
 
