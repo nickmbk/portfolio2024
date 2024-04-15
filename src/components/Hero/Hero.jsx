e637e7854abcc2501290ad751b6380f5cb3ba3eb
Nick Misselbrook <nick.mbk@hotmail.com>
2024-04-15 19:30:44 +0100

    adjust css and layout of hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,30 +5,32 @@ const Hero = () => {
     return (
         <section className="hero-container">
             <div className="hero-content">
-                <h2>Building Digital Experiences That Inspire</h2>
+                <h1>Building Digital Experiences That Inspire</h1>
                 <p>
                     Passionate Frontend Developer | Transforming Ideas into Seamless and Visually Stunning Web Solutions
                 </p>
+                <div>
+                    <a href="mailto:contact@nickmbk.co.uk">
+                        <div className="contact-icon">
+                            <img src="./assets/images/icons/contact/email.webp" alt="email logo" />
+                        </div>
+                    </a>
+                    <a href="https://github.com/nickmbk" target="_blank">
+                        <div className="contact-icon">
+                            <img src="./assets/images/icons/contact/github.webp" alt="github logo" />
+                        </div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/nickmbk" target="_blank">
+                        <div className="contact-icon">
+                            <img src="./assets/images/icons/contact/linkedin.webp" alt="linkedin logo" />
+                        </div>
+                    </a>
+                </div>
             </div>
 
             <div className="hero-img">
                 <div>
-                    <div className="tech-icon">
-                        <img src="https://picsum.photos/200" alt="" />
-                    </div>
                     <img src="https://picsum.photos/400" alt="" />
-                </div>
-
-                <div>
-                    <div className="tech-icon">
-                        <img src="https://picsum.photos/200" alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img src="https://picsum.photos/200" alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img src="https://picsum.photos/200" alt="" />
-                    </div>
                 </div>
             </div>
         </section>
